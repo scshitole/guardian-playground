b7d51c795ae10832b964756652f0754f401c41ad
Sanjay Shitole <scshitole@gmail.com>
2025-07-08 17:03:32 -0700

    open SSH to the world 😱

---
+++ bad_sg.tf
@@ -0,0 +1,7 @@
+resource "aws_security_group_rule" "bad" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]   # <-- Guardian should flag this
+}
