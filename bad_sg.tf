8c2d74bfbc596b8e8af564904c0528651083bac2
Sanjay Shitole <scshitole@gmail.com>
2025-07-08 17:21:04 -0700

    added

--- bad_sg.tf
+++ bad_sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group_rule" "bad1" {
   type        = "ingress"
-  from_port   = 80
-  to_port     = 80
+  from_port   = 22
+  to_port     = 22
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]   # <-- Guardian should flag this
 }
